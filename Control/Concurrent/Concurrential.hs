1c661160c3bcfdb315e504d0a9ef5bb05e3ae130
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-04-02 14:03:41 -0400

    Document Concurrential

--- Control/Concurrent/Concurrential.hs
+++ Control/Concurrent/Concurrential.hs
@@ -25,6 +25,7 @@ import Control.Applicative
 import Control.Monad
 import Control.Concurrent.Async hiding (concurrently)
 
+-- | Description of the way in which an IO should be carried out.
 data Choice t = Sequential (IO t) | Concurrent (IO t)
 
 instance Functor Choice where
@@ -53,6 +54,9 @@ instance Monad Concurrential where
   return = pure
   (>>=) = SCBind
 
+-- | Run a Concurrential term with a continuation. We choose CPS here because
+--   it allows us to explot @withAsync@, giving us a guarantee that an
+--   exception in a spawning thread will kill spawned threads.
 runConcurrentialK :: Concurrential t -> (Async t -> IO r) -> IO r
 runConcurrentialK sc k = case sc of
     SCAtom choice -> case choice of
@@ -70,12 +74,38 @@ runConcurrentialK sc k = case sc of
               return $ f x
         in withAsync waitAndApply k
 
+-- | Run a Concurrential term, realizing the effects of the IOs which compose
+--   it.
 runConcurrential :: Concurrential t -> IO t
 runConcurrential c = runConcurrentialK c wait
 
+-- | Create an IO which must be run sequentially.
+--   If a @sequentially io@ appears in a @Concurrential t@ term then it will
+--   always be run to completion before any later part of the term is run.
+--   Consider the following terms:
+--
+--   @
+--     a = someConcurrential *> sequentially io *> someOtherConcurrential
+--     b = someConcurrential *> concurrently io *> someOtherConcurrential
+--   @
+--
+--   When running the term @a@, we are guaranteed that @io@ is completed before
+--   @someOtherConcurrential@ is begun, but when running the term @b@, this is
+--   not the case; @io@ may be interleaved with or even run after
+--   @someOtherConcurrential@ is run.
 sequentially :: IO t -> Concurrential t
 sequentially = SCAtom . Sequential
 
+-- | Create an IO which is run concurrently where possible, i.e. whenever it
+--   combined applicatively with other terms. For instance:
+--
+--   @
+--     a = concurrently io *> someConcurrential
+--     b = concurrently io >> someConcurrential
+--   @
+--
+--   When running the term @a@, the IO term @io@ will be run concurrently with
+--   @someConcurrential@, but not so in @b@, because monadic composition has
+--   been used.
 concurrently :: IO t -> Concurrential t
 concurrently = SCAtom . Concurrent
-
